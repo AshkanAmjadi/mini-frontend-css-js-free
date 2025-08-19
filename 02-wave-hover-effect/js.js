9d66c0757f0811d5f3e8d4c1ad235830bebbea58
Ashkan Amjadi <Ashkan.amjadi8412@gmail.com>
2025-08-19 11:01:16 +0330

    add new 🎊 wave hover effect

---
+++ 02-wave-hover-effect/js.js
@@ -0,0 +1,31 @@
+
+window.addEventListener("load", (e) => {
+  document.querySelectorAll(".container .box").forEach((box) => {
+
+
+    
+
+    box.addEventListener("mouseenter", (e) => {
+      e.target.classList.add("hover");
+      let prev = e.target.previousElementSibling;
+      let prev2 = prev?.previousElementSibling;
+
+      let next = e.target.nextElementSibling;
+      let next2 = next?.nextElementSibling;
+
+      prev?.classList.add("p1");
+      prev2?.classList.add("p2");
+      next?.classList.add("n1");
+      next2?.classList.add("n2");
+    });
+    box.addEventListener("mouseleave", (e) => {
+      document.querySelectorAll(".hover,.p1,.p2,.n1,.n2").forEach((el) => {
+        el.classList.remove("hover", "p1", "p2", "n1", "n2");
+      });
+    });
+
+  
+  });
+});
+
+
